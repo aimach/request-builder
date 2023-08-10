cc629a627e6fee2e3a6c1e793dd8005eb8ede27f
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-08-10 15:51:33 +0200

    improve modal style and background style

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,13 +3,22 @@ import { infos, translation } from "./utils/data";
 import PropTypes from "prop-types";
 
 export default function Modal({ setShowModal, modalContent, lang }) {
-  const information = infos.filter((el) => el.name === modalContent);
   const english = lang === "en";
+  console.log(infos.fr);
 
   return (
     <div className="background-modal">
       <div className="content-modal flex-column">
-        <p>{english ? information[0].en : information[0].fr}</p>
+        <h3>
+          {english
+            ? infos.en[modalContent].title
+            : infos.fr[modalContent].title}
+        </h3>
+        <p>
+          {english
+            ? infos.en[modalContent].content
+            : infos.fr[modalContent].content}
+        </p>
         <button
           type="button"
           onClick={() => setShowModal(false)}
